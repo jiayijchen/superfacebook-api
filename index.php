8545d00a3c20a2626d8247c0f416759c041a0615
c.jiayichen <c.jiayichen@gmail.com>
2021-11-04 23:42:48 -0400

    added search hero action

--- index.php
+++ index.php
@@ -15,52 +15,27 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// IF there is an action, get it anc handle case
-if (isset($_GET["action"])) {
-    $action = $_GET["action"];
-    switch ($action) {
-        case "search":
-            echo "READ $text HEROES";
-            readAllHeroes();
-            break;
-        case "create":
-            // echo "CREATE A HERO";
-            createHero();
-            break;
-        case "update":
-            echo "Update hero";
-            // updateHero($_GET["id"], $_GET["name"], $_GET["tagline"]);
-            break;
-        case "delete":
-            echo "delete hero";
-            // deleteHero($_GET["id"]);
-            break;
-        default:
-            echo "Error 420: There is no action.";
-    }
-} else {
-    return;
-}
-
-// 
-
 // Read all heroes
 function readAllHeroes()
 {
     global $conn;
     $sql = "SELECT * FROM heroes";
     $result = $conn->query($sql);
-    $rows = array();
-
+    
     if ($result->num_rows > 0) {
         // output data of each row
+        $rows = array();
         while ($row = $result->fetch_assoc()) {
             $rows[] = $row;
-            echo "id: " . $row["id"] . "<br>
-                  name: <b>" . $row["name"] . "</b><br>
-                  about: <i>" . $row["about_me"] . "</i><br>
-                  biography: <i>" . $row["biography"] . "</i><br><br>";
+            // echo "id: " . $row["id"] . "<br>
+            //       name: <b>" . $row["name"] . "</b><br>
+            //       about: <i>" . $row["about_me"] . "</i><br>
+            //       biography: <i>" . $row["biography"] . "</i><br><br>";
+            //print json_encode($row);
         }
+        toJson($rows);
+        // echo json_encode($rows);
+        
     } else {
         echo "0 results";
     }
@@ -70,41 +45,81 @@ function readAllHeroes()
 function createHero()
 {
     global $conn;
+
+    // Check for correct parameters to create
     if (!isset($_POST["name"]) || !isset($_POST["about_me"]) || !isset($_POST["biography"]) || !isset($_POST["ability"])) {
-        echo "Error: Missing parameters to create hero.";
+        echo "Error: Missing parameter(s) to create hero.";
         return;
     }
+
+    // Set values
     $name = $_POST["name"];
     $aboutme = $_POST["about_me"];
     $biography = $_POST["biography"];
     $ability = $_POST["ability"];
-    $hero_id = -1;
-    $ability_id = -1;
 
-    $sqlHeroes = "INSERT INTO heroes (name, about_me, biography) VALUES ('$name', '$aboutme', '$biography')";
-    $sqlAbilities = "INSERT INTO ability_type (ability) VALUES ('$ability')";
+    // Set initial SQL query
+    $sql = "INSERT INTO heroes (name, about_me, biography) VALUES ('$name', '$aboutme', '$biography')";
 
-    if ($conn->query($sqlHeroes) === TRUE) {
+    // Test query, if succesful, pull created id
+    if ($conn->query($sql) === TRUE) {
         $hero_id = $conn->insert_id;
-        echo "$hero_id";
+        $ability_id = -1;
+        $sql = "SELECT id FROM ability_type WHERE ability='$ability';";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $ability_id = $row["id"];
+        } else { 
+            // ability_type doesn't exist, so insert
+            $sql = "INSERT INTO ability_type (ability) VALUES ('$ability')";
+            if ($conn->query($sql) === TRUE) {
+                $ability_id = $conn->insert_id;
+            } else { 
+                // error: pre-existing ability (should never happen with out if-statement)
+                echoError($sql, $conn->error);
+            }
+        }
 
-        if ($conn->query($sqlAbilities) === TRUE) {
-            $ability_id = $conn->insert_id;
-            echo "ability_id";
-        } else { // Ability_type already exists, so select
-            $sqlAbilities = "SELECT id FROM ability_type WHERE ability='$ability';";
-            $result = ($conn->query($sqlAbilities))->fetch_assoc();
-            $ability_id = $result["id"];
-            echo "ability_id";
-        }
-        $sqlHeroAbility = "INSERT INTO abilities (hero_id, ability_id) VALUES ('$hero_id', '$ability_id');";
-        if ($conn->query($sqlHeroAbility) === TRUE) {
+        $sql = "INSERT INTO abilities (hero_id, ability_id) VALUES ('$hero_id', '$ability_id');";
+        if ($conn->query($sql) === TRUE) {
             echo "New record created successfully";
-        } else {
-            echo "Error with SQL query: <br><br>" . $sqlHeroAbility . "<br><br>" . $conn->error . ".";
+        } else { 
+            // error: pre-existing hero-ability-relationship (should never happen)
+            echoError($sql, $conn->error);
         }
+    } else { 
+        // error: duplicate entry
+        echoError($sql, $conn->error);
+    }
+}
+
+// Search a hero
+function searchHero()
+{
+    global $conn;
+    if (!isset($_GET["name"])) {
+        echo "Error: Missing name parameter to search for a hero.";
+    }
+    $name = $_GET["name"];
+
+    $sql = "SELECT heroes.name, heroes.about_me, heroes.biography, GROUP_CONCAT(ability_type.ability) AS abilities
+            FROM abilities
+            INNER JOIN heroes ON heroes.id=abilities.hero_id
+            LEFT JOIN ability_type ON ability_type.id=abilities.ability_id
+            WHERE heroes.name='$name'
+            GROUP BY heroes.id";
+
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $rows = array();
+        while ($row = $result->fetch_assoc()) {
+            $rows[] = $row;
+        }
+        toJson($rows);
     } else {
-        echo "Error with SQL query: <br><br>" . $sqlHeroes . "<br><br>" . $conn->error . ".";
+        echo "<h3> 0 search results </h3>";
     }
 }
 
@@ -112,12 +127,27 @@ function createHero()
 function updateHero()
 {
     global $conn;
-    if (!isset($_POST["name"]) || !isset($_POST["about_me"]) || !isset($_POST["biography"])) {
-        echo "Error: Missing parameters to create hero.";
+
+    if (!isset($_POST["name"])) {
+        echo "Error: Missing name parameter to update a hero.";
         return;
+    } 
+
+    if (!isset($_POST["about_me"]) && !isset($_POST["biography"]) && !isset($_POST["ability"])) {
+        echo "Error: Missing parameter(s) to update hero.";
     }
+    
+    
+    $sql = "SELECT * FROM heroes WHERE";
 }
 
+function toJson($jsonstring) {
+    echo "<pre style='word-wrap: break-word; white-space: pre-wrap;'>" . json_encode($jsonstring) . "</pre>";
+}
+
+function echoError($sql, $error) {
+    echo "Error with SQL query: <br><br>" . $sql . "<br><br>" . $error . ".";
+}
 // function updateHero($id, $name, $tagline)
 // {
 
@@ -173,6 +203,33 @@ function updateHero()
 //     }
 
 // }
+
+if (isset($_GET["action"])) {
+    $action = $_GET["action"];
+    switch ($action) {
+        case "create":
+            createHero();
+            break;
+        case "search":
+            searchHero();
+            break;
+        case "readall":
+            readAllHeroes();
+            break;
+        case "update":
+            echo "Update hero";
+            // updateHero($_GET["id"], $_GET["name"], $_GET["tagline"]);
+            break;
+        case "delete":
+            echo "delete hero";
+            // deleteHero($_GET["id"]);
+            break;
+        default:
+            echo "Error 420: There is no action.";
+    }
+} else {
+    return;
+}
 
 // 
 $conn->close();
